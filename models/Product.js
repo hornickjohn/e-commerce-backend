02c54151e28e676a2e03d2c463c78521d530de67
John Hornick <jhornick@live.com>
2023-02-07 13:49:33 -0500

    Fixed product association fk

--- models/Product.js
+++ models/Product.js
@@ -28,14 +28,6 @@ Product.init(
       validate: {
         isNumeric: true
       }
-    },
-    category_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: 'category',
-        key: 'id',
-        onDelete: 'cascade'
-      }
     }
   },
   {
